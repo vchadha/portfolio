1d6af29ef77a9c00f11a99d01c4b411498adfb70
Varun Chadha <vichadha@protonmail.com>
2026-03-14 22:03:05 -0700

    Revert "Simplify navigation logic"
    This reverts commit 7e8552d9afb94e174b13a5ae350de536f71c5e3f.

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <a href="#nav" className="sr-only focus:not-sr-only">
         Skip to navigation
       </a>
-      <div className="flex flex-col md:flex-row min-h-screen w-full overflow-y-auto">
+      <div className="flex flex-col md:flex-row min-h-screen w-full">
         <ProfileAside />
         <DetailsSection />
       </div>
