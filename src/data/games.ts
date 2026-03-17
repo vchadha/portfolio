a4a929ad0fcb1b80b882be913dcbf4a4ff6000c1
Varun Chadha <vichadha@protonmail.com>
2026-03-16 18:05:31 -0700

    Update game images to gameplay

--- src/data/games.ts
+++ src/data/games.ts
@@ -21,9 +21,8 @@ export const games: Game[] = [
     image: "/images/games/eecs-survival.png",
     imageAspect: "cover",
     imageAlt:
-      "EECS Survival Menu Screen. A pixel art style menu with the following options: \
-      Start (play game), Suite (run test suite), Manual (manually test gameplay mechanics \
-      in sandbox), Auto (fuzzy testing), Quit.",
+      "EECS Survival battle screenshot. A 2D top-down pixel art game with a \
+      player character dodging paper balls.",
     jam: "Software Engineering · 2019",
     role: "Engine Lead",
     authors: [
@@ -49,7 +48,9 @@ export const games: Game[] = [
     image: "/images/games/sprawl.png",
     imageAspect: "cover",
     imageAlt:
-      "SPRAWL Menu Screen. The words SPRAWL a city builder over a landscape.",
+      "SPRAWL screenshot. A 3D city builder game where the player is placing buildings \
+      to construct a town. There is a phone in the bottom right for the in game simulated \
+      social media.",
     jam: "Graduate Project · 2021",
     role: "Team Lead",
     authors: ["Varun Chadha", "Amelia Moser", "Aidan Myers", "Jonah Tostrud"],
@@ -97,8 +98,8 @@ export const games: Game[] = [
     image: "/images/games/dino-soar.png",
     imageAspect: "cover",
     imageAlt:
-      "Dino Soar Title Screen. A side-scrolling platformer with a dinosaur character. \
-      The title 'Dino Soar' is displayed in large pixel art font with dinosaurs peeking out.",
+      "Dino Soar level screenshot. A dinosaur is navigating a 2D side facing level, \
+      dodging dinosaurs to retrieve its egg.",
     jam: "DINOJAM3 · 2023",
     authors: ["Justin Roderman", "Varun Chadha", "Sean Poole", "Alex Shadley"],
     myName: "Varun Chadha",
@@ -114,7 +115,8 @@ export const games: Game[] = [
     image: "/images/games/tick-tick-za.png",
     imageAspect: "cover",
     imageAlt:
-      "Tick Tick Za! Title Screenshot. Menu that says press enter to start.",
+      "Tick Tick Za! level screenshot. There is a conveyor belt carrying pizza dough down \
+      the screen, and the player is grabbing ingredients from stations to make pizzas.",
     jam: "Ludum Dare 51 · 2022",
     authors: ["Justin Roderman", "Varun Chadha", "Sean Poole"],
     myName: "Varun Chadha",
@@ -128,10 +130,10 @@ export const games: Game[] = [
     title: "Lost & Raturn",
     url: "https://justinro-underscore.itch.io/lost-raturn",
     repo: "https://github.com/justinro-underscore/Lost-Raturn",
-    image: "/images/games/lost-raturn.jpg",
+    image: "/images/games/lost-raturn.png",
     imageAspect: "cover",
     imageAlt:
-      "Lost & Raturn Title Screenshot. A side scrolling 2D pixel art game with a rat character.",
+      "Lost & Raturn level screenshot. A 2D rat is running up the legs of a man to return an item he dropped.",
     jam: "Global Game Jam · 2021",
     authors: ["Justin Roderman", "Varun Chadha", "Sean Poole"],
     myName: "Varun Chadha",
