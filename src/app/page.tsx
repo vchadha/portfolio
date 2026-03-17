ed53a87ad7a4a64cb807e441e5f6e5d104174e8e
Varun Chadha <vichadha@protonmail.com>
2026-03-16 17:21:31 -0700

    Add tab order to main content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
         <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 md:px-12 md:py-16 lg:py-0">
           <div className="lg:flex lg:justify-between lg:gap-4">
             <ProfileAside />
-            <main id="content" className="pt-24 lg:w-[52%] lg:py-24">
+            <main id="content" tabIndex={-1} className="pt-24 lg:w-[52%] lg:py-24">
               <About />
               <Experience />
               <Projects />
