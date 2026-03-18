d10d16fda265f83bb5e41f26e0e8ea8a2596b3af
Varun Chadha <vichadha@protonmail.com>
2026-03-17 19:46:31 -0700

    Add sword divider above footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,6 +35,7 @@ export default function Home() {
             <Experience />
             <PixelDivider />
             <Projects />
+            <PixelDivider />
             <Footer />
           </main>
         </div>
